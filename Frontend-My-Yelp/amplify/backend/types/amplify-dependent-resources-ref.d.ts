d23e0aec2bec3988420a45a288b3bee10af50962
Zokirbek Narzulla ogli To'laboyev <152845502+ZokirbekTolaboyev@users.noreply.github.com>
2023-12-29 14:55:59 +0500

    Yelp clone
    my_yelp finished

---
+++ Frontend-My-Yelp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplify4fbd1932": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ Frontend-My-Yelp/src/README_PROJECT_.md
@@ -0,0 +1,19 @@
+# Welcome to My Instagram
+Thank you
+
+# Task
+
+Had some issues with the aws amplify api
+
+# Description
+In My yelp: Login and Signup you password. 
+restaurant name, description and city name can be added to the api
+
+# Installation
+npm install
+
+# Usage
+npm start
+
+# The Core Team
+Sayyora Nuriddinova
