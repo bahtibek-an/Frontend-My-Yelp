05de6045cc8e0e2dbe1acb2339d014cf7d3f6710
User <aziztohtabaev24@gmail.com>
2024-01-30 15:24:22 +0500

    finished

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,46 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateRestaurant = /* GraphQL */ `
+  subscription OnCreateRestaurant(
+    $filter: ModelSubscriptionRestaurantFilterInput
+  ) {
+    onCreateRestaurant(filter: $filter) {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateRestaurant = /* GraphQL */ `
+  subscription OnUpdateRestaurant(
+    $filter: ModelSubscriptionRestaurantFilterInput
+  ) {
+    onUpdateRestaurant(filter: $filter) {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteRestaurant = /* GraphQL */ `
+  subscription OnDeleteRestaurant(
+    $filter: ModelSubscriptionRestaurantFilterInput
+  ) {
+    onDeleteRestaurant(filter: $filter) {
+      id
+      name
+      description
+      city
+      createdAt
+      updatedAt
+    }
+  }
+`;
