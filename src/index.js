c02c872a2dc594cb0ee7d431c6228a98916b7273
User <muhammadnurullaev10@gmail.com>
2024-03-06 05:56:16 +0500

    finish all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+Amplify.configure(config);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
