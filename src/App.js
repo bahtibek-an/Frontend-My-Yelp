87f91bd93285d5baee6029fca6c3ccef3122663c
sevaaaar <sevaranorbutayeva829@gmail.com>
2024-03-26 14:08:20 +0500

    fixed 404 error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./firebase/config";
 import "./App.css"
 import Home from "./Home/Home";
 import Login from "./css/components/Login";
-// import Register from "./components/Register";
+import Register from "./css/components/Register";
 
 function App() {
-  const [login, setIsLoggin] = useState(false);
+  const [isLogin, setIsLoggin] = useState(false);
 
   const auth = getAuth();
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     let findOut = onAuthStateChanged(auth, (user) => {
       if (user) {
         setIsLoggin(true);
+        navigate('/')
       } else {
         setIsLoggin(false);
+        navigate('/login')
       }
     });
     return findOut;
@@ -29,7 +33,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/register" element={<Register />} /> */}
+          <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
